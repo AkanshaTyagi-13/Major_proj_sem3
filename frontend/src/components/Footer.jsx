2a3302ba2357bc304c6a49938fdc85073f2781e0
Akshita Gupta <akshita.2426mca209@kiet.edu>
2025-09-10 18:31:10 +0530

    Delete frontend directory

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className='bg-gray-800 text-white text-center py-3 mt-8'>
-      <p>© 2025 Internship Finder. All rights reserved.</p>
-    </footer>
-  );
-}
